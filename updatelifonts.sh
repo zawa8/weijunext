6967957dce962d14e344cd8736efae99494ed4f7
viml kumar <vimalkg@gmail.com>
2025-03-10 00:26:33 +0530

    mck82.vercel.app zikis8.vercel.app fb.com/ztrabc

---
+++ updatelifonts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+############################
+vrclpath="/home/viml/mg/vrcl/"
+weijunext_componentspath="/home/viml/mg/vrcl/weijunext/components/"
+declare -a vrcl_arr=(
+"/home/viml/mg/vrcl/mck_/mck82/components/"
+"/home/viml/mg/vrcl/mck_/mck81/components/"
+"/home/viml/mg/vrcl/zikis8/components/"
+"/home/viml/mg/vrcl/shenlu/components/" 
+"/home/viml/mg/vrcl/vrclblog/src/app/_components/"
+"/home/viml/mg/vrcl/vord8/components/"
+"/home/viml/mg/vrcl/nxbunquiz/src/app/Components/"
+"/home/viml/mg/vrcl/chensonexsupabasesaasboiler/components/"
+"/home/viml/mg/vrcl/fpnxcustbill/app/ui/"
+"/home/viml/mg/vrcl/chatgpt_next_web/app/components/"
+"/home/viml/mg/vrcl/leeorb_mdx_blogsite/app/"
+"/home/viml/mg/vrcl/leeshadcn/components/"
+)
+############################
+for i in "${vrcl_arr[@]}"
+do
+	rm -r ${i}/lifonts
+	cp -r ${weijunext_componentspath}/lifonts ${i}/
+done
+############################
